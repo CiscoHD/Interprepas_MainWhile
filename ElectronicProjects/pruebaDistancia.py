c0c1acafaf3c099e67aa4378c86d18b7d3714901
Erickdced <32326086@alumnp.enp.unam.mx>
2025-03-10 17:29:21 -0500

    prueba distancia m+as precisa

--- ElectronicProjects/pruebaDistancia.py
+++ ElectronicProjects/pruebaDistancia.py
@@ -5,7 +5,11 @@ sensor = gpio.DistanceSensor(echo=23, trigger=24)
 
 try:
     while True:
-        print(sensor.distance)
+        if sensor.distance > 0.09 and sensor.distance < 0.30:
+            x = sensor.distance7
+            x *= 100
+            porcentaje = (21-(30-x))*100/21
+            print("porcentaje:", porcentaje)
         sleep(1)
 except KeyboardInterrupt:
     print("Exiting...")
