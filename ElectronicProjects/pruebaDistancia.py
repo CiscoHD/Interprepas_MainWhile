32f238b3c29053e496fbb1f56e80309a0f078cbc
Erickdced <32326086@alumnp.enp.unam.mx>
2025-03-10 17:30:07 -0500

    error de dedo corregido

--- ElectronicProjects/pruebaDistancia.py
+++ ElectronicProjects/pruebaDistancia.py
@@ -6,7 +6,7 @@ sensor = gpio.DistanceSensor(echo=23, trigger=24)
 try:
     while True:
         if sensor.distance > 0.09 and sensor.distance < 0.30:
-            x = sensor.distance7
+            x = sensor.distance
             x *= 100
             porcentaje = (21-(30-x))*100/21
             print("porcentaje:", porcentaje)
